59ccf78b18d5920b817a42dd673b4b55d9bc1a90
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2024-05-29 21:03:06 -0300

    Atualização do Modelo Fisico

--- Modelo FIsico.sql
+++ Modelo FIsico.sql
@@ -40,45 +40,32 @@ CREATE TABLE pastel_recheios(
     CONSTRAINT fk_pastel_recheios_pasteis FOREIGN KEY (id_pasteis) REFERENCES pasteis(id_pasteis)
 );
 
-CREATE TABLE tipo_bebidas(
-	id_tipo_bebidas INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(50) NOT NULL
+CREATE TABLE produtos(
+	id_produtos INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    bebidas VARCHAR(50) NOT NULL,
+    sobremesas VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE bebidas(
-	id_bebidas INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(50) NOT NULL,
-    id_tipo_bebidas INT NOT NULL,
-    preco DECIMAL(8,2) NOT NULL,
-    CONSTRAINT fk_bebidas_tipos_bebidas FOREIGN KEY (id_tipo_bebidas) REFERENCES tipo_bebidas(id_tipo_bebidas)
-);
-
-CREATE TABLE sobremesas(
-	id_sobremesa INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(50) NOT NULL,
-    preco DECIMAL (8,2) NOT NULL
-);
-
-CREATE TABLE forma_pagamento(
-	id_forma_pagamento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(50) NOT NULL
+CREATE TABLE itens_pedido(
+	id_itens_pedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_produtos INT NOT NULL,
+    id_pedido INT NOT NULL,
+    id_tamanho_pastel INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_total DECIMAL(8,2) NOT NULL,
+    CONSTRAINT fk_itens_pedido_produtos FOREIGN KEY (id_produtos) REFERENCES produtos(id_produtos),
+    CONSTRAINT fk_itens_pedido_pedido FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedidos),
+    CONSTRAINT fk_itens_pedido_tamanho_pastel FOREIGN KEY (id_tamanho_pastel) REFERENCES tamanho_pasteis(id_tamanho_pasteis)
 );
 
 CREATE TABLE pedidos(
 	id_pedidos INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_cliente INT NOT NULL,
-    id_forma_pagamento INT NOT NULL,
-    id_sobremesa INT NOT NULL,
-    id_bebida INT NOT NULL,
-    id_pastel INT NOT NULL,
+    id_clientes INT NOT NULL,
     data_pedido DATE NOT NULL,
-    CONSTRAINT fk_pedidos_clientes FOREIGN KEY (id_cliente) REFERENCES clientes(id_clientes),
-    CONSTRAINT fk_pedidos_forma_pagamento FOREIGN KEY (id_forma_pagamento) REFERENCES forma_pagamento(id_forma_pagamento),
-    CONSTRAINT fk_pedidos_sobremesas FOREIGN KEY (id_sobremesa) REFERENCES sobremesas(id_sobremesa),
-    CONSTRAINT fk_pedidos_bebidas FOREIGN KEY (id_bebida) REFERENCES bebidas(id_bebidas),
-    CONSTRAINT fk_pedidos_pasteis FOREIGN KEY (id_pastel) REFERENCES pasteis(id_pasteis)
+    forma_pagamento VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_pedidos_clientes FOREIGN KEY (id_clientes) REFERENCES clientes(id_clientes)
 );
-
+    
 
 
 
